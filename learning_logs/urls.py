c9e717d17c12732c0a95b39211a9bc1ca360329a
Kayode Odole <odoleemma@gmail.com>
2020-09-24 00:58:17 +0100

    I added base.html, topic.html, topics.html for viewing the topics and their entries.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,4 +5,8 @@ app_name = 'learning_logs' #namespace for the app
 
 urlpatterns= [
     path('', views.index, name='index' ),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    # Page that shows details for a topic
+    path('topics/<int:topic_id>', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render,HttpResponse
+from learning_logs.models import Topic, Entry
 
 
 #homepage
 def index(request):
-    return HttpResponse("Hello world")
+    '''The home page for Learning Log.'''
+    return render( request, 'learning_logs/index.html')
+
+def topics( request):
+    '''show all topics '''
+    # query the database for topics and sort by the date_added attribute
+    topics = Topic.objects.order_by('date_added')
+    # a dictionary in which keys are names I used in the template to access data
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    '''show a single topic and all its entries.'''
+    topic = Topic.objects.get(id=topic_id)
+    # the '-' in front of the date_added argument, makes django to 
+    # sort the entries by reverse order( latest to earliest )
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, 'entries':entries}
+    return render(request, 'learning_logs/topic.html', context)
