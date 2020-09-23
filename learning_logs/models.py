ec4daf0034615ce97fc4bee9eb0dd7006c00582f
Kayode Odole <odoleemma@gmail.com>
2020-09-23 22:22:31 +0100

    I created the Topic model in learning_logs

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Topic( models.Model):
+    '''A topic the user is learning about '''
+    #store the text representing the topic
+    text = models.CharField(max_length=200) 
+
+    #record date and time the topic was created( auto_now_add argument
+    # tells django to automatically set this attribute to the current
+    # date and time whenever the user creates a new topic)
+    date_added= models.DateTimeField( auto_now_add = True)
+    
+
+    def __str__(self):
+        '''Return a string representation of the model. '''
+        return self.text
