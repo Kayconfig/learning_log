4f6a9749d10027eb63827a908017cfdc25fb913e
Kayode Odole <odoleemma@gmail.com>
2020-09-23 22:45:08 +0100

    Migrated the Entry model, and registered the model to admin site

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from learning_logs.models import Topic
+from learning_logs.models import Topic,Entry
 
 #register the topic model, so that the admin page can manage the model
-admin.site.register(Topic)
-
+admin.site.register( Topic )
+admin.site.register( Entry )
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,13 +4,37 @@ class Topic( models.Model):
     '''A topic the user is learning about '''
     #store the text representing the topic
     text = models.CharField(max_length=200) 
-
-    #record date and time the topic was created( auto_now_add argument
-    # tells django to automatically set this attribute to the current
-    # date and time whenever the user creates a new topic)
-    date_added= models.DateTimeField( auto_now_add = True)
     
+    #record the date and time the entry was created ( auto_now_add argument
+    # tells django to fill this field automatically. )
+    date_added= models.DateTimeField( auto_now_add = True)
+
 
     def __str__(self):
         '''Return a string representation of the model. '''
         return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    # create a relationship between Entry and Topic.
+    # Many Entries can have one topic : Many-to-one relationship.
+    # models.CASCADE argument makes django to delete all entries associated 
+    # to a topic once the topic is deleted. => a.k.a cascading delete
+    topic = models.ForeignKey( Topic, on_delete= models.CASCADE)
+
+    text = models.TextField() #content of the entry
+    #record the date and time the entry was created ( auto_now_add argument
+    # tells django to fill this field automatically. )
+    date_added = models.DateTimeField( auto_now_add = True )
+
+    #holds extra information for managing Entry model.
+    #verbose_name_plural simply tells django to use 'Entries' when referring
+    #to more than one entry. Otherwise django would use 'Entrys'.
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.text[:50]}..."
+
+
