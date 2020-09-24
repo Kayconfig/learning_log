2708eaae27d318db5958e987e18dd81eaa350448
Kayode Odole <odoleemma@gmail.com>
2020-09-24 03:08:06 +0100

    Successfully customized login page

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,15 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
+     #my apps
+    'learning_logs',
+    'users',
+
+    #django default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #my apps
-    'learning_logs',
-    'users'
+   
+    
 ]
 
 MIDDLEWARE = [
